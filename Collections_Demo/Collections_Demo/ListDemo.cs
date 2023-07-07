a90e4e50cad97cf50e7318847d03fd4ead39186b
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-07-07 08:55:06 +0530

    [Sandeep] Add : List collections

---
+++ Collections_Demo/Collections_Demo/ListDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Demo
+{
+    //List<T> class represents the list of objects which can be accessed with index
+    //List is Dynamic
+    
+    public class ListDemo
+    {
+        //Creating a list of integers
+        List<int> list = new List<int>();
+        public void Adding()
+        {
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Count is: " +list.Count);
+            Console.WriteLine("List of Capacity: " + list.Capacity);
+            list.Add(20);
+            list.Add(30);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Count is: " + list.Count);
+            Console.WriteLine("List of Capacity: " + list.Capacity);
+            Console.WriteLine("Element present: "+ list.Contains(40));
+            Console.WriteLine("Remove element: " + list.Remove(20));
+            list.RemoveAt(2);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+            list.Reverse();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+            list.Add(11);
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+            list.Clear();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Collections_Demo/Collections_Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Demo
+{
+    internal class Program
+    {
+        //Collection is a class that means we can create an object
+        //can be create or declared instance of a class,so we can add a data in collection
+        //collection types - are designed to store, manage similar data
+        //We can used to adding, remove, find and sorting data in collection
+        //two types: Generic and Non-Generic collections
+        //Non-generic - arrayList, HashTable, sortedList, stack and Queue
+        //Generic : List, dictionary, sortedlist, stack and queue
+        static void Main(string[] args)
+        {
+            ListDemo listDemo = new ListDemo();
+            listDemo.Adding();
+
+            Console.ReadLine();
+        }
+    }
+}
